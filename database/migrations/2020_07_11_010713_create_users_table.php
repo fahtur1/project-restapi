9f1e46e6cd2e7311bce0b7176e9b27e4c12c3c95
fahtur1 <fahturrahman18ti@mahasiswa.pcr.ac.id>
2020-07-11 13:22:33 +0700

    Change Column db

--- database/migrations/2020_07_11_010713_create_users_table.php
+++ database/migrations/2020_07_11_010713_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email');
             $table->string('password');
-            $table->string('api_token');
             $table->timestamps();
         });
     }
