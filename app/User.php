1aab7b5ddf906a781668af03739f8c76a0b030a8
fahtur1 <fahturrahman18ti@mahasiswa.pcr.ac.id>
2020-07-12 00:17:03 +0700

    Remove column api_token

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'password', 'api_token'
+        'password'
     ];
 
     public $incrementing = false;
